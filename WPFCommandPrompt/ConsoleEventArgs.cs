d82c61a2353bea8c103edb766de5ec89a6f75611
Adrian Hum <adrian@littlegreenmartian.net>
2016-01-06 18:39:25 +1100

    Changes made to UI Code and Demo Project
    Signed-off-by: Adrian Hum <adrian@littlegreenmartian.net>

--- WPFCommandPrompt/ConsoleEventArgs.cs
+++ WPFCommandPrompt/ConsoleEventArgs.cs
@@ -1,108 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Media;
-using System.Windows.Documents;
+﻿// Author: Adrian Hum
+// Project: WPFCommandPrompt/ConsoleEventArgs.cs
+// 
+// Created : 2016-01-06  18:29 
+// Modified: 2016-01-06 18:35)
 
-namespace WPFCommandPrompt
-{
+using System;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WPFCommandPrompt {
     /// <summary>
-    /// Delegate for the write Event Handler.
+    ///     Delegate for the write Event Handler.
     /// </summary>
     /// <param name="sender">The sender.</param>
-    /// <param name="e">The <see cref="WPFCommandPrompt.ConsoleWriteLineEventArgs"/> instance containing the event data.</param>
+    /// <param name="e">The <see cref="WPFCommandPrompt.ConsoleWriteLineEventArgs" /> instance containing the event data.</param>
     public delegate void WriteLineEventHandler(object sender, ConsoleWriteLineEventArgs e);
 
     /// <summary>
-    /// Event arguments for a console write event.
+    ///     Event arguments for a console write event.
     /// </summary>
-    public class ConsoleWriteLineEventArgs : EventArgs
-    {
+    public class ConsoleWriteLineEventArgs : EventArgs {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConsoleWriteLineEventArgs"/> class.
+        ///     Text color.
+        /// </summary>
+        public readonly Brush Foreground;
+
+        /// <summary>
+        ///     Message string to/from the console.
+        /// </summary>
+        public readonly string Message;
+
+        /// <summary>
+        ///     The flow document paragraph to write to the console.
+        /// </summary>
+        public readonly Paragraph Paragraph;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ConsoleWriteLineEventArgs" /> class.
         /// </summary>
         /// <param name="message">Message to send to the console.</param>
         public ConsoleWriteLineEventArgs(string message)
         {
-            this.Message = message;
+            Message = message;
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConsoleWriteLineEventArgs"/> class.
+        ///     Initializes a new instance of the <see cref="ConsoleWriteLineEventArgs" /> class.
         /// </summary>
         /// <param name="message">>Message to send to the console.</param>
         /// <param name="foreground">Text color (overrides default).</param>
         public ConsoleWriteLineEventArgs(string message, Brush foreground)
         {
-            this.Message = message;
-            this.Foreground = foreground;
+            Message = message;
+            Foreground = foreground;
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConsoleWriteLineEventArgs"/> class.
+        ///     Initializes a new instance of the <see cref="ConsoleWriteLineEventArgs" /> class.
         /// </summary>
         /// <param name="paragraph">The paragraph to write to the console.</param>
         public ConsoleWriteLineEventArgs(Paragraph paragraph)
         {
-            this.Paragraph = paragraph;
+            Paragraph = paragraph;
         }
-
-        /// <summary>
-        /// The flow document paragraph to write to the console.
-        /// </summary>
-        public readonly Paragraph Paragraph;
-
-        /// <summary>
-        /// Text color.
-        /// </summary>
-        public readonly Brush Foreground;
-
-        /// <summary>
-        /// Message string to/from the console.
-        /// </summary>
-        public readonly string Message;
     }
 
     /// <summary>
-    /// Delegate for the read Event Handler.
+    ///     Delegate for the read Event Handler.
     /// </summary>
     /// <param name="sender">The sender.</param>
-    /// <param name="e">The <see cref="WPFCommandPrompt.ConsoleReadLineEventArgs"/> instance containing the event data.</param>
+    /// <param name="e">The <see cref="WPFCommandPrompt.ConsoleReadLineEventArgs" /> instance containing the event data.</param>
     public delegate void ReadLineEventHandler(object sender, ConsoleReadLineEventArgs e);
 
     /// <summary>
-    /// Event arguments for a console read event as a parsed array of
-    /// commands or a single unparsed string of commands.
+    ///     Event arguments for a console read event as a parsed array of
+    ///     commands or a single unparsed string of commands.
     /// </summary>
-    public class ConsoleReadLineEventArgs : EventArgs
-    {
+    public class ConsoleReadLineEventArgs : EventArgs {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConsoleReadLineEventArgs"/> class.
-        /// </summary>
-        /// <param name="commands">The parsed commands as an array.</param>
-        public ConsoleReadLineEventArgs(string[] commands)
-        {
-            this.Commands = commands;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ConsoleReadLineEventArgs"/> class.
-        /// </summary>
-        /// <param name="commandString">The un-parsed command string.</param>
-        public ConsoleReadLineEventArgs(string commandString)
-        {
-            this.CommandString = commandString;
-        }
-
-        /// <summary>
-        /// Parsed string array of commands
+        ///     Parsed string array of commands
         /// </summary>
         public readonly string[] Commands;
 
         /// <summary>
-        /// Un-parsed command string.
+        ///     Un-parsed command string.
         /// </summary>
         public readonly string CommandString;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ConsoleReadLineEventArgs" /> class.
+        /// </summary>
+        /// <param name="commands">The parsed commands as an array.</param>
+        public ConsoleReadLineEventArgs(string[] commands)
+        {
+            Commands = commands;
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ConsoleReadLineEventArgs" /> class.
+        /// </summary>
+        /// <param name="commandString">The un-parsed command string.</param>
+        public ConsoleReadLineEventArgs(string commandString)
+        {
+            CommandString = commandString;
+        }
     }
 }
--- WPFCommandPrompt/Properties/AssemblyInfo.cs
+++ WPFCommandPrompt/Properties/AssemblyInfo.cs
@@ -1,10 +1,16 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// Author: Adrian Hum
+// Project: WPFCommandPrompt/AssemblyInfo.cs
+// 
+// Created : 2016-01-06  18:29 
+// Modified: 2016-01-06 18:35)
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("WPF Command Prompt")]
 [assembly: AssemblyDescription("A Flexible WPf Command Prompt")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +23,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("16e5b30b-1ac5-454f-8b67-219faae6b5ef")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +40,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.11235.1704")]
 [assembly: AssemblyFileVersion("1.0.11235.1704")]
